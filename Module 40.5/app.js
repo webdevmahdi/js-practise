d12d61fcd4d4908f001ed4265098bdc62700f1da
webdevmahdi <mahdihasan01705259@gmail.com>
2023-07-24 08:54:58 +0600

    updaded

--- Module 40.5/app.js
+++ Module 40.5/app.js
@@ -1,4 +1,4 @@
-let i = 0;
+/* let i = 0;
 setInterval(myFunc, 3500);
 function myFunc(){
     console.log('Mahdi', i++);
@@ -8,4 +8,10 @@ let inputNum = parseInt(input);
 if(input > 5){
     let result = 200 + inputNum;
     alert(result)
+} */
+
+let href = 'My name is mahdi';
+let ask = confirm(href);
+if(ask === true){
+    console.log(href);
 }
