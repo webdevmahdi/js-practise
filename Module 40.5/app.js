4d17e786a5017f0b9d17a256f019bf5469debafd
webdevmahdi <mahdihasan01705259@gmail.com>
2023-07-24 08:45:21 +0600

    update

--- Module 40.5/app.js
+++ Module 40.5/app.js
@@ -1,6 +1,11 @@
 let i = 0;
-
 setInterval(myFunc, 3500);
 function myFunc(){
     console.log('Mahdi', i++);
+}
+let input = prompt('type a number');
+let inputNum = parseInt(input);
+if(input > 5){
+    let result = 200 + inputNum;
+    alert(result)
 }
